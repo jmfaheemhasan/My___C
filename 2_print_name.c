85ac922bf7d4f2580084d8c1f6852e44492416a0
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-09-05 10:20:45 +0600

    2_print_name.c

---
+++ 2_print_name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // input-output functions ব্যবহার করার জন্য
+
+int main() {
+    printf("My name is Fahim Hasan\n");  // টেক্সট প্রিন্ট করবে
+    return 0;   // প্রোগ্রাম সফলভাবে শেষ হবে
+}
